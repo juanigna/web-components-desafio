f9e6337d2d112f1cc706a196449e193554fcf6da
JuaniSO <juan.bocchi@sphereone.xyz>
2023-09-27 16:03:21 -0300

    menu responsive finished

--- components/header.js
+++ components/header.js
@@ -11,8 +11,48 @@ function headerComponent(element) {
                 <li>Servicios</li>
                 <li>Contacto</li>
             </ul>
+            <img src="../assets/menu.svg" class="nav-right-side-menu"/>
+            <div class="nav-right-side-menu-container">
+                <img src="../assets/close.svg" class="nav-right-side-menu-close"/>
+                <ul class="nav-right-side-items-mobile">
+                    <li class="nav-right-side-items-mobile-item">Portfolio</li>
+                    <li class="nav-right-side-items-mobile-item">Servicios</li>
+                    <li class="nav-right-side-items-mobile-item">Contacto</li>
+                </ul>
+            </div>
+
         </div>
     `
 
+    const abrir = componentMain.querySelector('.nav-right-side-menu')
+    const cerrar = componentMain.querySelector('.nav-right-side-menu-close')
+    const menuMobile = componentMain.querySelector('.nav-right-side-menu-container')
+
+
+
+    abrir?.addEventListener('click', () => {
+        menuMobile.style.display = "flex"
+    })
+
+    cerrar?.addEventListener('click', () => {
+        menuMobile.style.display = "none"
+    })
+
+    // Add a listener to detect when the viewport size changes
+    const mediaQuery = window.matchMedia('(max-width: 768px)'); // Adjust the max-width value as needed
+
+    function handleViewportChange(e) {
+        if (!e.matches) {
+
+            // If viewport width is greater than 768px, hide mobile menu
+            menuMobile.style.display = "none";
+        }
+    }
+
+    mediaQuery.addEventListener("change", handleViewportChange); // Add listener
+    handleViewportChange(mediaQuery);
+
+
     element.appendChild(componentMain)
 }
+
